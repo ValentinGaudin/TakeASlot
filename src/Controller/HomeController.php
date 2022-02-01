aa0c8cee45ba618671119de9625f440a2878f139
Valentin <valentingaudin@gmail.com>
2022-02-01 14:05:28 +0100

    First Commit and prepar the repo

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,13 +3,9 @@
 namespace App\Controller;
 
 use Exception;
-use App\Entity\Category;
-use App\Form\SearchProductType;
-use App\Repository\ProductRepository;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * This will suppress all the PMD warnings in
@@ -22,111 +18,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        $form = $this->createForm(SearchProductType::class);
-
-        $search = $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $category = $search->get('category')->getData();
-            if (!$category instanceof Category) {
-                throw new Exception('Category not found');
-            }
-
-            return $this->redirectToRoute(
-                'product_search',
-                [
-                    'request' => $request
-                ],
-                307
-            );
-        }
-
         return $this->render('home/index.html.twig', [
-            'form' => $form->createView()
+            'controller_name' => 'HomeController',
         ]);
-    }
-
-    /**
-     * @Route("/search", name="product_search", methods={"GET", "POST"})
-     */
-    public function searchFromProductPage(
-        Request $request,
-        ProductRepository $productRepository
-    ): Response {
-        $form = $this->createForm(SearchProductType::class);
-
-        $search = $form->handleRequest($request);
-        $products = [];
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $category = $search->get('category')->getData();
-            $minWidth = $search->get('minWidth')->getData();
-            $minDepth = $search->get('minDepth')->getData();
-            $minHeight = $search->get('minHeight')->getData();
-            $maxWidth = $search->get('maxWidth')->getData();
-            $maxDepth = $search->get('maxDepth')->getData();
-            $maxHeight = $search->get('maxHeight')->getData();
-            $price = intval($search->get('price')->getData());
-
-            $searchParameters = [
-                'category' => $category,
-                'minWidth' => $minWidth,
-                'minDepth' => $minDepth,
-                'minHeight' => $minHeight,
-                'maxWidth' => $maxWidth,
-                'maxDepth' => $maxDepth,
-                'maxHeight' => $maxHeight,
-                'price' => $price,
-                'page' => 1
-            ];
-
-            $products = $productRepository->searchProduct($searchParameters);
-        }
-
-        return $this->render('home/search.html.twig', [
-            'form' => $form->createView(),
-            'products' => $products,
-        ]);
-    }
-
-    /**
-     * @Route("/ajax/{page}/{category}/{minHeight}/{maxHeight}/{minWidth}/{maxWidth}/{minDepth}/{maxDepth}/{price}"),
-     * name="ajax_request", method="POST"
-     */
-    public function ajaxProduct(
-        int $page,
-        int $category,
-        int $minHeight,
-        int $maxHeight,
-        int $minWidth,
-        int $maxWidth,
-        int $minDepth,
-        int $maxDepth,
-        int $price,
-        ProductRepository $productRepository
-    ): Response {
-        $searchParameters = [
-            'category' => $category,
-            'minWidth' => $minWidth,
-            'minDepth' => $minDepth,
-            'minHeight' => $minHeight,
-            'maxWidth' => $maxWidth,
-            'maxDepth' => $maxDepth,
-            'maxHeight' => $maxHeight,
-            'price' => $price,
-            'page' => $page
-        ];
-
-        $products = $productRepository->searchProduct($searchParameters);
-        $productsCount = $productRepository->countSearchProduct($searchParameters);
-
-        return new Response($this->render('home/_productsDisplay.html.twig', [
-            'search' => $searchParameters,
-            'productsCount' => $productsCount,
-            'products' => $products,
-            'page' => $page,
-        ]));
     }
 }
