3b49cb8dc230701572aceaa731d79731a647b130
Valentin <valentingaudin@gmail.com>
2022-02-02 09:10:47 +0100

    Ready for CP4

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,10 @@
 
 namespace App\Controller;
 
-use Exception;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-/**
- * This will suppress all the PMD warnings in
- * this class.
- *
- * @SuppressWarnings(PHPMD)
- */
 class HomeController extends AbstractController
 {
     /**
@@ -20,8 +13,7 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('calendar/calendar.html.twig', [
         ]);
     }
 }
